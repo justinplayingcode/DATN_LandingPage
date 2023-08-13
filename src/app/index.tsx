d85199ec9d764db44d9bf9614461eba43b34484e
justin.pham <phamduythang00@gmail.com>
2023-08-13 14:48:38 +0700

    scroll to top

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,19 +6,22 @@ import About from './page/About';
 import Services from './page/Service';
 import News from './page/News';
 import Contact from './page/Contact';
+import ScrollToTop from './structure/ScrollWrapper';
 
 const App: React.FunctionComponent = () => {
 
   return (
     <HashRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="home" element={<Layout content={<Home/>}/>} />
-        <Route path="/about" element={<Layout content={<About/>}/>} />
-        <Route path="/service" element={<Layout content={<Services/>}/>} />
-        <Route path="/news" element={<Layout content={<News/>}/>} />
-        <Route path="/contact" element={<Layout content={<Contact/>}/>} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" replace />} />
+          <Route path="home" element={<Layout content={<Home/>}/>} />
+          <Route path="/about" element={<Layout content={<About/>}/>} />
+          <Route path="/service" element={<Layout content={<Services/>}/>} />
+          <Route path="/news" element={<Layout content={<News/>}/>} />
+          <Route path="/contact" element={<Layout content={<Contact/>}/>} />
+        </Routes>
+      </ScrollToTop>
     </HashRouter>
   )
 }
