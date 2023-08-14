3c884f4fffb3647cd18b64be4235eb326385513a
justin.pham <phamduythang00@gmail.com>
2023-08-14 20:00:21 +0700

    news

---
+++ src/app/mockdata.ts
@@ -0,0 +1,30 @@
+export const newsPosts = [
+  {
+    image: "https://res.cloudinary.com/dipiauw0v/image/upload/v1691845344/DATN/phau_thuat-18_06_58_183_ibgsoe.jpg",
+    title: "Cảnh báo hóc dị vật vào phổi khi nuốt dị vật",
+    subtitle: "Hóc dị vật là một trong những hiện tượng khá điển hình trong cuộc sống. Hầu hết các tình huống hóc không nhiều nguy hiểm, và có thể được xử lý tại chỗ",
+    author: "Nguyễn Thị Cẩm Tú",
+    date: "08/08/2023",
+  },
+  {
+    image: "https://res.cloudinary.com/justinpham311/image/upload/v1691947580/hospital-cloud-post/gan-nhiem-mo-gay-nhieu-tac-hai-doi-voi-nguoi-benh_ymlio8.jpg",
+    title: "Gan nhiễm mỡ có tác hại gì và cách kiểm soát",
+    subtitle: "Gan là một tạng lớn trong cơ thể đảm nhận các chức năng như tổng hợp, chuyển hoá, giải độc và dự trữ. Trong gan luôn tồn tại một lượng mỡ nhất định, thường từ 2 – 4% trọng lượng lá gan.",
+    author: "Trần Thị Thoa",
+    date: "06/08/2023",
+  },
+  {
+    image: "https://res.cloudinary.com/dipiauw0v/image/upload/v1691845344/DATN/phau_thuat-18_06_58_183_ibgsoe.jpg",
+    title: "Cảnh báo hóc dị vật vào phổi khi nuốt dị vật",
+    subtitle: "Hóc dị vật là một trong những hiện tượng khá điển hình trong cuộc sống. Hầu hết các tình huống hóc không nhiều nguy hiểm, và có thể được xử lý tại chỗ",
+    author: "Nguyễn Thị Cẩm Tú",
+    date: "08/08/2023",
+  },
+  {
+    image: "https://res.cloudinary.com/dipiauw0v/image/upload/v1691845344/DATN/phau_thuat-18_06_58_183_ibgsoe.jpg",
+    title: "Cảnh báo hóc dị vật vào phổi khi nuốt dị vật",
+    subtitle: "Hóc dị vật là một trong những hiện tượng khá điển hình trong cuộc sống. Hầu hết các tình huống hóc không nhiều nguy hiểm, và có thể được xử lý tại chỗ",
+    author: "Nguyễn Thị Cẩm Tú",
+    date: "08/08/2023",
+  },
+]
